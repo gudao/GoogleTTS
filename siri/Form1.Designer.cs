c450cbc19e76f341b74f7e8ed48fec255a186938
gudao <gudao2008@gmail.com>
2012-11-05 09:00:39 +0800

    init

---
+++ siri/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace siri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBowser = new System.Windows.Forms.Button();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.rbEnglish = new System.Windows.Forms.RadioButton();
+            this.rbChinese = new System.Windows.Forms.RadioButton();
+            this.tbHz = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.ListBox();
+            this.lbpro = new System.Windows.Forms.Label();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btOutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btBowser
+            // 
+            this.btBowser.Location = new System.Drawing.Point(390, 14);
+            this.btBowser.Name = "btBowser";
+            this.btBowser.Size = new System.Drawing.Size(62, 23);
+            this.btBowser.TabIndex = 1;
+            this.btBowser.Text = "浏览";
+            this.btBowser.UseVisualStyleBackColor = true;
+            this.btBowser.Click += new System.EventHandler(this.btBowser_Click);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Location = new System.Drawing.Point(101, 16);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(283, 21);
+            this.tbFilePath.TabIndex = 0;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(309, 85);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(75, 23);
+            this.btSend.TabIndex = 5;
+            this.btSend.Text = "开始识别";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // rbEnglish
+            // 
+            this.rbEnglish.AutoSize = true;
+            this.rbEnglish.Checked = true;
+            this.rbEnglish.Location = new System.Drawing.Point(101, 58);
+            this.rbEnglish.Name = "rbEnglish";
+            this.rbEnglish.Size = new System.Drawing.Size(47, 16);
+            this.rbEnglish.TabIndex = 2;
+            this.rbEnglish.TabStop = true;
+            this.rbEnglish.Text = "英文";
+            this.rbEnglish.UseVisualStyleBackColor = true;
+            // 
+            // rbChinese
+            // 
+            this.rbChinese.AutoSize = true;
+            this.rbChinese.Location = new System.Drawing.Point(154, 58);
+            this.rbChinese.Name = "rbChinese";
+            this.rbChinese.Size = new System.Drawing.Size(47, 16);
+            this.rbChinese.TabIndex = 3;
+            this.rbChinese.Text = "中文";
+            this.rbChinese.UseVisualStyleBackColor = true;
+            // 
+            // tbHz
+            // 
+            this.tbHz.Location = new System.Drawing.Point(101, 93);
+            this.tbHz.Name = "tbHz";
+            this.tbHz.Size = new System.Drawing.Size(75, 21);
+            this.tbHz.TabIndex = 4;
+            this.tbHz.Text = "16000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "语  言：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "采样率：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "（默认16000HZ）";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "文  件：";
+            // 
+            // tbResult
+            // 
+            this.tbResult.FormattingEnabled = true;
+            this.tbResult.ItemHeight = 12;
+            this.tbResult.Location = new System.Drawing.Point(45, 136);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.tbResult.Size = new System.Drawing.Size(407, 280);
+            this.tbResult.TabIndex = 11;
+            // 
+            // lbpro
+            // 
+            this.lbpro.AutoSize = true;
+            this.lbpro.Location = new System.Drawing.Point(45, 423);
+            this.lbpro.Name = "lbpro";
+            this.lbpro.Size = new System.Drawing.Size(23, 12);
+            this.lbpro.TabIndex = 12;
+            this.lbpro.Text = "0/0";
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(390, 85);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(62, 23);
+            this.btStop.TabIndex = 13;
+            this.btStop.Text = "停止";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // btOutput
+            // 
+            this.btOutput.Location = new System.Drawing.Point(376, 423);
+            this.btOutput.Name = "btOutput";
+            this.btOutput.Size = new System.Drawing.Size(75, 23);
+            this.btOutput.TabIndex = 14;
+            this.btOutput.Text = "导出全文";
+            this.btOutput.UseVisualStyleBackColor = true;
+            this.btOutput.Click += new System.EventHandler(this.btOutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 479);
+            this.Controls.Add(this.btOutput);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.lbpro);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHz);
+            this.Controls.Add(this.rbChinese);
+            this.Controls.Add(this.rbEnglish);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.btBowser);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "声音识别";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBowser;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.RadioButton rbEnglish;
+        private System.Windows.Forms.RadioButton rbChinese;
+        private System.Windows.Forms.TextBox tbHz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox tbResult;
+        private System.Windows.Forms.Label lbpro;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btOutput;
+    }
+}
+
---
+++ siri/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+namespace siri
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+        private StringBuilder sbResult = new StringBuilder();
+        public Form1()
+        {
+            InitializeComponent();
+            btStop.Enabled = false;
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(DoFor);
+            worker.ProgressChanged += new ProgressChangedEventHandler(ChangeFor);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(CompletedFor);
+        }
+
+        private void DoFor(object sender, DoWorkEventArgs e)
+        {
+            
+            bool isEnglish = rbEnglish.Checked;
+            string rate = tbHz.Text.Trim();
+
+            int postLength = 100 * 1024;
+            byte[] postData = File.ReadAllBytes(e.Argument.ToString());
+            int count = postData.Length / postLength;
+            int yu = postData.Length % postLength;
+            
+            for (int i = 0; i < count; i++)
+            {
+                byte[] temp = new byte[postLength];
+                Array.Copy(postData, i * postLength, temp, 0, postLength);
+                ChangeResult rs = new ChangeResult();
+                rs.Res=convertFlac(temp, rate, isEnglish);
+                rs.Pro=i + "/" + count;
+                worker.ReportProgress(0, rs);
+                if (worker.CancellationPending)
+                { break; }
+            }
+            if (yu > 0&&!worker.CancellationPending)
+            {
+                byte[] temp = new byte[yu];
+                Array.Copy(postData, count * postLength, temp, 0, yu);
+                ChangeResult rs = new ChangeResult();
+                rs.Res = convertFlac(temp, rate, isEnglish);
+                rs.Pro = "完成！";
+                worker.ReportProgress(0, rs);
+            }
+            if (worker.CancellationPending)
+            {
+                worker.ReportProgress(0, new ChangeResult() {  Pro="终止！",Res=""});
+            }
+            
+        }
+        public void ChangeFor(object sender, ProgressChangedEventArgs e)
+        {
+           ChangeResult res=  e.UserState as ChangeResult;
+           if (res.Res != "")
+           {
+               tbResult.Items.Add(res.Res);
+               sbResult.Append(res.Res);
+           }
+           lbpro.Text = res.Pro;
+
+        }
+        public void CompletedFor(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btSend.Text = "开始识别";
+            btSend.Enabled = true;
+            
+        }
+
+
+        private void btBowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbFilePath.Text = ofd.FileName;
+            }
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbFilePath.Text.Trim()) && !string.IsNullOrEmpty(tbHz.Text.Trim()))
+            {
+                if (!File.Exists(tbFilePath.Text.Trim()))
+                    return;
+
+                lbpro.Text = "0/0";
+                btSend.Text = "识别中...";
+                btSend.Enabled = false;
+                btStop.Enabled = true;
+                worker.RunWorkerAsync(tbFilePath.Text.Trim());
+            }
+        }
+
+        private string convertFlac(byte[] postData,string rate,bool isEnglish)
+        {
+            try
+            {
+                string url = "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-US&maxresults=1";
+                if (!isEnglish) url = "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=zh-CN&maxresults=1";
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.ServicePoint.Expect100Continue = false;
+                req.Method = "POST";
+                req.KeepAlive = true;
+                req.UserAgent = "adks";
+                //req.Timeout = 400000;
+                req.ContentType = "audio/x-flac; rate=" + rate;
+                Stream reqStream = req.GetRequestStream();
+                reqStream.Write(postData, 0, postData.Length);
+                reqStream.Close();
+
+                HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
+                Encoding encoding = Encoding.GetEncoding(rsp.CharacterSet);
+                reqStream = rsp.GetResponseStream();
+                StreamReader reader = new StreamReader(reqStream, encoding);
+                string jsonresult = reader.ReadToEnd();
+
+                GoogleResponse objResult =
+               JsonConvert.DeserializeObject<GoogleResponse>(jsonresult);
+                if (objResult.status == 0 && objResult.hypotheses.Length > 0)
+                {
+                    return objResult.hypotheses[0].utterance;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception ee)
+            {
+                return "服务器错误！请查看google是否能够正常访问！" + ee;
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            worker.CancelAsync();
+            btStop.Enabled = false;
+        }
+
+        private void btOutput_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                sfd.RestoreDirectory = true;
+                sfd.Filter = "*.txt|*.txt|所有文件(*.*)|*.*";
+                System.IO.File.WriteAllText(sfd.FileName, sbResult.ToString());
+                sbResult.Clear();
+                tbResult.Items.Clear();
+            }
+        }
+    }
+
+    public class ChangeResult
+    {
+        public string Res { get; set; }
+        public string Pro { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class GoogleHypothesa
+    {
+        [JsonProperty]
+        public string utterance { get; set; }
+
+        [JsonProperty]
+        public double confidence { get; set; }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class GoogleResponse
+    {
+        [JsonProperty]
+        public int status { get; set; }
+        [JsonProperty]
+        public string id { get; set; }
+        [JsonProperty]
+        public GoogleHypothesa[] hypotheses { get; set; }
+    }
+}
